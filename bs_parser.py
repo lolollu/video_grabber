c3c7c62aa6bb9f7fcb17dc2cba9c19426067b53c
simon <jiahao@lightchaseranimation.com>
2017-05-24 14:29:48 +0800

    1

--- bs_parser.py
+++ bs_parser.py
@@ -20,6 +20,14 @@ request_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11
                   'Referer': None,
                   }
 
+request_header_common = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+                  'Accept': 'text/html;q=0.9,*/*;q=0.8',
+                  'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+                  'Accept-Encoding': 'gzip',
+                  'Connection': 'close',
+                  'Referer': None,
+                  }
+
 class ProgressBar(object):
     def __init__(self, title, count=0.0, run_status=None, fin_status=None, total=100.0, unit='', sep='/',
                  chunk_size=1.0):
